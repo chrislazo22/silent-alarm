f7290699c92d357f75dd41117c04de54fbd6ca71
Chris Lazo <lazochristian.cl@gmail.com>
2025-05-07 12:19:40 -0700

    Initial Commit

---
+++ WatchAlarm/WatchAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchAlarmApp.swift
+//  WatchAlarm
+//
+//  Created by Christian Lazo on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct WatchAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
